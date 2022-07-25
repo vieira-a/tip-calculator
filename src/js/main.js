499db6602c640370dfef5e90623acd8ecaa23eed
Anderson Vieira <asvieira.br@gmail.com>
2022-07-25 16:06:37 -0300

    Capturar o valor do botão clicado de acordo com o Select Tip, ou do valor customizado através do input

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,15 @@ function getBillValue() {
   let billValue = Number(document.querySelector('#bill').value)
   return billValue
 }
+
+function selectTip(clickedTip) {
+  if (document.querySelector('#custom').value === '') {
+    let tip = Number(clickedTip)
+    return tip
+    //console.log(tip)
+  } else {
+    tip = Number(document.querySelector('#custom').value)
+    return tip
+    //console.log(tip)
+  }
+}
