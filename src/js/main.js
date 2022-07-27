1f7ddaf0a98e5f4509ff911a864f01f9c1409d52
Anderson Vieira <asvieira.br@gmail.com>
2022-07-27 17:09:19 -0300

    Aplicação de estilo do design mobile: divs, fonts, position, etc

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,17 @@
 let bill = 0
 let numberOfpeople = 0
 
-document.querySelector('#bill').addEventListener("change", function() {
+document.querySelector('#bill').addEventListener('change', function () {
   bill = Number(document.querySelector('#bill').value)
   console.log(bill)
 })
 
-document.querySelector('#number-of-people').addEventListener("change", function() {
-  numberOfpeople = Number(document.querySelector('#number-of-people').value)
-  console.log(numberOfpeople)
-})
+document
+  .querySelector('#number-of-people')
+  .addEventListener('change', function () {
+    numberOfpeople = Number(document.querySelector('#number-of-people').value)
+    console.log(numberOfpeople)
+  })
 
 let tip = ''
 function getTip(clickedTip) {
@@ -18,17 +20,20 @@ function getTip(clickedTip) {
 }
 
 function calculateAmount() {
-  let amountTip = bill*(tip/100)
-  let amountTipByPerson = amountTip/numberOfpeople
+  let amountTip = bill * (tip / 100)
+  let amountTipByPerson = amountTip / numberOfpeople
   let amount = bill + amountTip
-  let amountByPerson = amount/numberOfpeople
-  
-  document.getElementById('tip-by-person').textContent = `R$ ${amountTipByPerson.toFixed(2)}`
-  document.getElementById('total-by-person').textContent = `R$ ${amountByPerson.toFixed(2)}`
+  let amountByPerson = amount / numberOfpeople
+
+  document.getElementById(
+    'tip-by-person'
+  ).textContent = `R$ ${amountTipByPerson.toFixed(2)}`
+  document.getElementById(
+    'total-by-person'
+  ).textContent = `R$ ${amountByPerson.toFixed(2)}`
 }
 
 function getNumberPeople() {
   let people = document.querySelector('#number-of-people').value
   return people
-  //console.log(people)
 }
